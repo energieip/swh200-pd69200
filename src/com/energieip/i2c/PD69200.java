b8e20c82a1c9a3cb13c8e31e7a2137f8567069e3
stef <stef@Gilgamesh>
2019-08-13 20:06:19 +0200

    Pd69200 format

--- src/com/energieip/i2c/PD69200.java
+++ src/com/energieip/i2c/PD69200.java
@@ -14,7 +14,7 @@ public class PD69200 implements Runnable {
 	private I2CBus bus;
 	private I2CBus i2c;
 	private I2CDevice device;
-	
+
 	// PD69200 I2C address
 	private final int PD69200_ADDR = 0x3C;
 	private final int NUMBER_OF_LOGICAL_PORTS = 24;
@@ -28,44 +28,42 @@ public class PD69200 implements Runnable {
 	// thread
 	Thread readThread;
 	private int SCAN_RATE = 1000; // in milliseconds
-	
+
 	// const
 	final int POWER_MAX = 6000;
 	final int POWER_MIN = 0;
-	
-
 
 	/**
 	 * Default constructor
-	 * @param i2c_bus 
+	 * 
+	 * @param i2c_bus
 	 */
 	public PD69200(int _i2c_bus) {
-		
-		i2c_bus = _i2c_bus;
-	
-		
-			initi2c();
-			//resetPSE();
-			//initPSE();
 
-			//readThread = new Thread(this);
-			//readThread.start();
-		
+		i2c_bus = _i2c_bus;
+
+		initi2c();
+		// resetPSE();
+		// initPSE();
+
+		// readThread = new Thread(this);
+		// readThread.start();
+
 	} // end of constructor
 
 	/**
 	 * initialize PSE
 	 * 
 	 * @throws InterruptedException
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	private void initPSE() throws InterruptedException, IOException {
 		System.out.println("[WAIT] Initializing PSE...");
 
 		// enable channels
-		//pse_enable_channels(get_echo());
-		//Thread.sleep(50); // wait 50 ms
-		
+		// pse_enable_channels(get_echo());
+		// Thread.sleep(50); // wait 50 ms
+
 		byte[] tab = new byte[15]; // output buffer tab
 		tab = pse_get_software_version(get_echo());
 		System.out.println("\npse_get_sofware_version");
@@ -74,14 +72,13 @@ public class PD69200 implements Runnable {
 			int res = device.read(buf, 0, 1);
 			if (buf[0] != 0) {
 				int pos = device.read(buf, 1, 14);
-				System.out.println("POS="+ pos);
+				System.out.println("POS=" + pos);
 				break;
 			}
 		}
 		System.out.println("\npse_get_sofware_version");
 		printBuffer(buf);
 
-		
 		// enable 4 pairs and PoH
 		tab = new byte[15]; // output buffer tab
 		tab = pse_set_4_pair_ports_parameters(get_echo());
@@ -89,25 +86,23 @@ public class PD69200 implements Runnable {
 		printBuffer(tab);
 		device.write(tab);
 		Thread.sleep(50); // wait 50 ms
-		
+
 		tab = new byte[15]; // output buffer tab
 		tab = pse_get_4_pair_ports_parameters(get_echo());
 		device.write(tab);
-		//Thread.sleep(50); // wait 50 ms
-		
+		// Thread.sleep(50); // wait 50 ms
+
 		buf = new byte[15];
 		while (true) {
 			int res = device.read(buf, 0, 1);
 			if (buf[0] == 0x03) {
 				int pos = device.read(buf, 1, 14);
-				System.out.println("POS="+ pos);
+				System.out.println("POS=" + pos);
 				break;
 			}
 		}
 		System.out.println("\npse_get_4_pair_ports_parameters");
 		printBuffer(buf);
-
-		 
 
 		// enable new matrix (4 pairs)
 		tab = pse_set_individual_mask(get_echo());
@@ -115,64 +110,50 @@ public class PD69200 implements Runnable {
 		printBuffer(tab);
 		device.write(tab);
 		Thread.sleep(50); // wait 50 ms
-		
+
 		// set power limit to 62W (0XFF 0xFE)
-		//pse_set_4_pair_power_limit(get_echo());
-		//Thread.sleep(100); // wait 100 ms
-		
+		// pse_set_4_pair_power_limit(get_echo());
+		// Thread.sleep(100); // wait 100 ms
+
 		byte ll = 0x00;
 		for (int l = 0; l < NUMBER_OF_LOGICAL_PORTS; l++) {
-			tab = pse_set_temporary_matrix(get_echo(),ll);
+			tab = pse_set_temporary_matrix(get_echo(), ll);
 			device.write(tab);
 			ll++;
 			Thread.sleep(50); // wait 50 ms
 		}
-		
+
 		tab = pse_program_global_matrix(get_echo());
 		device.write(tab);
-		Thread.sleep(50); // wait 50 ms	
-		
+		Thread.sleep(50); // wait 50 ms
+
 		tab = pse_enable_4_pair_for_channels(get_echo());
 		device.write(tab);
 		Thread.sleep(50); // wait 50 ms
-		
-		
-		//pse_force_power(get_echo());
-		//Thread.sleep(100); // wait 100 ms
-		
+
+		// pse_force_power(get_echo());
+		// Thread.sleep(100); // wait 100 ms
+
 		/*
-		pse_get_power_limit(get_echo());
-		while (true) {
-			int res = device.read(buf, 0, 1);
-			if (buf[0] != 0) {
-				int pos = device.read(buf, 1, 14);
-				// System.out.println("go :" + i + " pos:" + pos);
-				break;
-			}
-		}
-		System.out.println("[ACK] get power limit ");
-		printBuffer(buf);
-		*/
-		
-		//pse_save_settings(get_echo());
-		//System.out.println("[WAIT] Waiting for system backup ACK...");
-		//Thread.sleep(50); // wait 50 ms
-		//int i = 1;
+		 * pse_get_power_limit(get_echo()); while (true) { int res =
+		 * device.read(buf, 0, 1); if (buf[0] != 0) { int pos = device.read(buf,
+		 * 1, 14); // System.out.println("go :" + i + " pos:" + pos); break; } }
+		 * System.out.println("[ACK] get power limit "); printBuffer(buf);
+		 */
+
+		// pse_save_settings(get_echo());
+		// System.out.println("[WAIT] Waiting for system backup ACK...");
+		// Thread.sleep(50); // wait 50 ms
+		// int i = 1;
 		/*
-		while (true) {
-			int res = device.read(buf, 0, 1);
-			if (buf[0] != 0) {
-				int pos = device.read(buf, 1, 14);
-				// System.out.println("go :" + i + " pos:" + pos);
-				break;
-			}
-		}
-		*/
+		 * while (true) { int res = device.read(buf, 0, 1); if (buf[0] != 0) {
+		 * int pos = device.read(buf, 1, 14); // System.out.println("go :" + i +
+		 * " pos:" + pos); break; } }
+		 */
 		System.out.println("[OK] System backup complete");
 		Thread.sleep(100); // wait 100 ms
 
 	}
-
 
 	/**
 	 * initialize I2C
@@ -183,9 +164,9 @@ public class PD69200 implements Runnable {
 			System.out.println("Initializing I2C...");
 			ids = I2CFactory.getBusIds();
 			System.out.println("Found follow I2C busses: " + Arrays.toString(ids));
-			if(i2c_bus==0){
+			if (i2c_bus == 0) {
 				bus = I2CFactory.getInstance(I2CBus.BUS_0);
-			} else if(i2c_bus==1) {
+			} else if (i2c_bus == 1) {
 				bus = I2CFactory.getInstance(I2CBus.BUS_1);
 			} else {
 				System.err.println("[ERROR], bad I2C bus");
@@ -204,25 +185,24 @@ public class PD69200 implements Runnable {
 		}
 	} // end of init
 
-	
 	private void resetPSE() throws InterruptedException, IOException {
 		System.out.println("[WAIT] resetting PSE...");
 		tab = new byte[15]; // output buffer tab
 		tab = pse_reset_to_factory_default(get_echo());
 		device.write(tab);
-		
+
 		Thread.sleep(100); // wait 100 ms
 	}
-	
+
 	private byte[] pse_set_temporary_matrix(byte get_echo, byte l) {
 
 		tab[0] = (byte) 0x00; // command
 		tab[1] = echo;
 		tab[2] = (byte) 0x05; // channel
 		tab[3] = (byte) 0x43; // temporary matrix
-		tab[4] =  l; // all ports
-		tab[5] = (byte) (l*(byte)2); 
-		tab[6] =  (byte) (l*(byte)2+(byte)1);
+		tab[4] = l; // all ports
+		tab[5] = (byte) (l * (byte) 2);
+		tab[6] = (byte) (l * (byte) 2 + (byte) 1);
 		tab[7] = (byte) 0x4E;
 		tab[8] = (byte) 0x4E;
 		tab[9] = (byte) 0x4E;
@@ -235,17 +215,17 @@ public class PD69200 implements Runnable {
 		tab = checksum(tab);
 
 		return tab;
-		
+
 	}
-	
+
 	private byte[] pse_program_global_matrix(byte get_echo) {
 
 		tab[0] = (byte) 0x00; // command
 		tab[1] = echo;
 		tab[2] = (byte) 0x07;
 		tab[3] = (byte) 0x43; // temporary matrix
-		tab[4] = (byte) 0x4E; 
-		tab[5] = (byte) 0x4E; 
+		tab[4] = (byte) 0x4E;
+		tab[5] = (byte) 0x4E;
 		tab[6] = (byte) 0x4E;
 		tab[7] = (byte) 0x4E;
 		tab[8] = (byte) 0x4E;
@@ -259,10 +239,9 @@ public class PD69200 implements Runnable {
 		tab = checksum(tab);
 
 		return tab;
-		
+
 	}
 
-	
 	/**
 	 * set PSE unlimited
 	 * 
@@ -291,14 +270,14 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_set_individual_mask(byte echo) {
 
 		tab[0] = (byte) 0x00; // command
 		tab[1] = echo;
-		tab[2] = (byte) 0x07; 
-		tab[3] = (byte) 0x56; 
-		tab[4] = (byte) 0x34; // 0x34: New matrix (4 pairs) 
+		tab[2] = (byte) 0x07;
+		tab[3] = (byte) 0x56;
+		tab[4] = (byte) 0x34; // 0x34: New matrix (4 pairs)
 		tab[5] = (byte) 0x01; // enable
 		tab[6] = (byte) 0x4E;
 		tab[7] = (byte) 0x4E;
@@ -315,15 +294,14 @@ public class PD69200 implements Runnable {
 		return tab;
 	}
 
-	
 	private byte[] pse_reset_to_factory_default(byte echo) {
 
 		tab[0] = (byte) 0x01; // program
 		tab[1] = echo;
-		tab[2] = (byte) 0x2D; 
-		tab[3] = (byte) 0x4E; 
-		tab[4] = (byte) 0x4E; 
-		tab[5] = (byte) 0x4E; 
+		tab[2] = (byte) 0x2D;
+		tab[3] = (byte) 0x4E;
+		tab[4] = (byte) 0x4E;
+		tab[5] = (byte) 0x4E;
 		tab[6] = (byte) 0x4E;
 		tab[7] = (byte) 0x4E;
 		tab[8] = (byte) 0x4E;
@@ -363,7 +341,7 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_enable_4_pair_for_channels(byte echo) {
 
 		tab[0] = (byte) 0x00; // command
@@ -372,12 +350,12 @@ public class PD69200 implements Runnable {
 		tab[3] = (byte) 0x02; // enable/disable 4 Pair
 		tab[4] = (byte) 0x80; // CH number or 0x80 for all
 		tab[5] = (byte) 0x01; // enable 4 pair (0x00 for disable)
-		tab[6] = (byte) 0x4E; 
-		tab[7] = (byte) 0x4E; 
-		tab[8] = (byte) 0x4E; 
-		tab[9] = (byte) 0x4E; 
-		tab[10] = (byte) 0x4E; 
-		tab[11] = (byte) 0x4E; 
+		tab[6] = (byte) 0x4E;
+		tab[7] = (byte) 0x4E;
+		tab[8] = (byte) 0x4E;
+		tab[9] = (byte) 0x4E;
+		tab[10] = (byte) 0x4E;
+		tab[11] = (byte) 0x4E;
 		tab[12] = (byte) 0x4E;
 		tab[13] = (byte) 0x00;
 		tab[14] = (byte) 0x00;
@@ -388,13 +366,12 @@ public class PD69200 implements Runnable {
 	}
 
 	private byte[] pse_set_4_pair_ports_parameters(byte echo) {
-		
+
 		byte[] tab_new = new byte[15]; // output buffer tab
 		/*
-		System.out.println("DEBUG0");
-		printBuffer(tab_new);
-		*/
-		
+		 * System.out.println("DEBUG0"); printBuffer(tab_new);
+		 */
+
 		tab_new[0] = (byte) 0x00; // command
 		tab_new[1] = echo;
 		tab_new[2] = (byte) 0x05; // channel
@@ -411,17 +388,17 @@ public class PD69200 implements Runnable {
 		tab_new[13] = (byte) 0x00;
 		tab_new[14] = (byte) 0x00;
 
-		//System.out.println("DEBUG1");
-		//printBuffer(tab_new);
-		
+		// System.out.println("DEBUG1");
+		// printBuffer(tab_new);
+
 		tab_new = checksum(tab_new);
-		
-		//System.out.println("DEBUG2");
-		//printBuffer(tab_new);
+
+		// System.out.println("DEBUG2");
+		// printBuffer(tab_new);
 
 		return tab_new;
 	}
-	
+
 	private byte[] pse_get_4_pair_ports_parameters(byte echo) {
 
 		tab[0] = (byte) 0x02; // command
@@ -444,7 +421,6 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-
 
 	private byte[] pse_set_4_pair_power_limit(byte echo) {
 
@@ -563,14 +539,14 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_get_software_version(byte echo) {
 
 		tab[0] = (byte) 0x02; // command
 		tab[1] = echo;
 		tab[2] = (byte) 0x07; // global
 		tab[3] = (byte) 0x1E; // VersionZ
-		tab[4] = (byte) 0x21; // SW Version 
+		tab[4] = (byte) 0x21; // SW Version
 		tab[5] = (byte) 0x4E;
 		tab[6] = (byte) 0x4E;
 		tab[7] = (byte) 0x4E;
@@ -747,7 +723,7 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_save_settings(byte echo) {
 
 		tab[0] = (byte) 0x01; // command
@@ -770,16 +746,16 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_enable_channels(byte echo) {
 
 		tab[0] = (byte) 0x00; // command
 		tab[1] = echo;
-		tab[2] = (byte) 0x05; //channel
+		tab[2] = (byte) 0x05; // channel
 		tab[3] = (byte) 0x0C;
 		tab[4] = (byte) 0x80;
 		tab[5] = (byte) 0x4E; // NIBBLES
-		tab[6] = (byte) 0x02; 
+		tab[6] = (byte) 0x02;
 		tab[7] = (byte) 0x4E;
 		tab[8] = (byte) 0x4E;
 		tab[9] = (byte) 0x4E;
@@ -793,15 +769,15 @@ public class PD69200 implements Runnable {
 
 		return tab;
 	}
-	
+
 	private byte[] pse_get_power_limit(byte echo) {
 
 		tab[0] = (byte) 0x02; // request
 		tab[1] = echo;
-		tab[2] = (byte) 0x05; 
+		tab[2] = (byte) 0x05;
 		tab[3] = (byte) 0x0B;
-		tab[4] = (byte) 0x01; // first channel 
-		tab[5] = (byte) 0x4E; 
+		tab[4] = (byte) 0x01; // first channel
+		tab[5] = (byte) 0x4E;
 		tab[6] = (byte) 0x4E;
 		tab[7] = (byte) 0x4E;
 		tab[8] = (byte) 0x4E;
@@ -817,7 +793,6 @@ public class PD69200 implements Runnable {
 		return tab;
 	}
 
-
 	/**
 	 * This method does the checksum
 	 * 
@@ -827,12 +802,12 @@ public class PD69200 implements Runnable {
 	private byte[] checksum(byte[] tab) {
 		int checksum = 0;
 		for (int i = 0; i < tab.length - 2; i++) {
-			//System.out.println("i=" + i + " checksum=" + checksum);
+			// System.out.println("i=" + i + " checksum=" + checksum);
 			checksum = checksum + tab[i];
 		}
 
 		short checksum_short = (short) checksum;
-		//System.out.println("checksum short=" + checksum_short);
+		// System.out.println("checksum short=" + checksum_short);
 
 		tab[14] = (byte) (checksum_short & 0xff);
 		tab[13] = (byte) ((checksum_short >> 8) & 0xff);
@@ -869,7 +844,7 @@ public class PD69200 implements Runnable {
 						// System.out.println("go :" + i + " pos:" + pos);
 						break;
 					}
-				}			
+				}
 				printBuffer(buf);
 				extractData(buf);
 				Thread.sleep(SCAN_RATE);
@@ -882,28 +857,26 @@ public class PD69200 implements Runnable {
 		}
 	} // end of thread
 
-	
 	private void extractData(byte[] buffer) {
-		if(buffer[0]==0x03){ // Telemetry
-			int power = ((buffer[2] & 0xff) << 8) | (buffer[3] & 0xff);	
-			if(power>POWER_MAX || power <POWER_MIN){
+		if (buffer[0] == 0x03) { // Telemetry
+			int power = ((buffer[2] & 0xff) << 8) | (buffer[3] & 0xff);
+			if (power > POWER_MAX || power < POWER_MIN) {
 				// do nothing
-			}else{
+			} else {
 				System.out.println("power= " + power + " W");
 			}
-			
+
 		}
-		
-		
+
 	}
 
 	private void printBuffer(byte[] buf2) {
 		System.out.println("**********************");
 		for (int j = 0; j < buf2.length; j++) {
-			System.out.println("buf["+j+"]=" + (buf2[j]));
+			System.out.println("buf[" + j + "]=" + (buf2[j]));
 		}
 		System.out.println("**********************");
-		
+
 	} // end of printBuffer
 
 } // end of class
