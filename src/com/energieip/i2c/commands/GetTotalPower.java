0892fbd16d9780ee6b135e6b10ebb3f9b0a96166
stef <stef@Gilgamesh>
2019-08-15 17:22:46 +0200

    correct an error in i2c bus definition in GetTotalPower

--- src/com/energieip/i2c/commands/GetTotalPower.java
+++ src/com/energieip/i2c/commands/GetTotalPower.java
@@ -20,8 +20,8 @@ public class GetTotalPower {
 	 * @param args
 	 */
 	private static void command(String[] args) {
-			PD69200 pd69200_1 = new PD69200(1);
-			PD69200 pd69200_2 = new PD69200(2);			
+			PD69200 pd69200_1 = new PD69200(0);
+			PD69200 pd69200_2 = new PD69200(1);			
 			int power = pd69200_1.pse_get_total_power() + pd69200_2.pse_get_total_power();
 			System.out.println(power);
 	}
